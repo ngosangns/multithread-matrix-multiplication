2632b7465703e64d38b9d5cee2c00b85c7bef49d
NGO SANG <ngosangns@gmail.com>
2021-04-16 19:35:37 +0700

    feat: Done presentation

---
+++ presentation/visualize_high.py
@@ -0,0 +1,34 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+f = open("experiments_high.txt","r")
+
+data = f.read().split("\n")
+data = [x.split(" ") for x in data]
+data = np.array(data).transpose()
+
+labels = data[0]
+single = data[1].astype(np.int32)
+multi = data[2].astype(np.int32)
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, single, width, label='Single-thread')
+rects2 = ax.bar(x + width/2, multi, width, label='Multi-thread')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Time (ms)')
+ax.set_title('Performance (high size matrices)')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+ax.bar_label(rects1, padding=3)
+ax.bar_label(rects2, padding=3)
+
+fig.tight_layout()
+
+plt.show()
